f26ccf89920570f6a68fcdeca331008bd4fcf3a5
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-11 16:36:55 +0200

    feat: add Bubble Sort implementation and main program for user input

---
+++ function_methodes_interfaces/bubble_sort/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func BubbleSort(numbers []int) {
+	length := len(numbers)
+	for i := 1; i < length; i++ {
+		for j := 0; j < (length - 1); j++ {
+			if numbers[j] > numbers[j+1] {
+				Swap(numbers, j)
+			}
+		}
+	}
+}
+
+func Swap(numbers []int, i int){
+	numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+}
+
+func main() {
+	integers := make([]int, 0)
+	var usrInput int
+	ran := []int{1, 2, 3, 4, -1, 0}
+	BubbleSort(ran)
+	fmt.Println("Blaaaaaaa: ", ran)
+	
+	fmt.Println("Program starting")
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Enter Integer (%v/10): ", i + 1)
+		fmt.Scan(&usrInput)
+		integers = append(integers, usrInput)
+	}
+	fmt.Println("final Slice of integers: ", integers)
+}
